4335fa9749dc85ca109516762e21388156fe8b29
Gevorah <jhonsebastianijajiortiz@gmail.com>
2020-03-19 12:57:19 -0500

    first commit

--- src/model/TuringMachine.java
+++ src/model/TuringMachine.java
@@ -2,54 +2,113 @@ package model;
 
 public class TuringMachine {
 	private Tape first;
+	private Tape c0;
+	private Tape c1;
+	private Tape c2;
 	private int n;
 	public TuringMachine() {
+		first=null;
+		c0=null;
+		c1=null;
+		c2=null;
 		n=-1;
 	}
-	public char read(int head) {
-		Tape tmp = first;
+	public void add(char head,char name) {
+		Tape add=new Tape(name);
+		if(first!=null) {
+			Tape curr=first;
+			if(head=='0') {
+				add.setNext(curr);
+				first=add;
+			}else if(head=='1') {
+				
+			}else if(head=='2') {
+				
+			}
+		}else {
+			first=add;
+			c0=first;
+			c1=first;
+			c2=first;
+		}
+	}
+	/*public char read(char head) {
+		Tape current = first;
 		int c1=n==0?0:n/2;
 		int c=0;
-		while(tmp!=null) {
-			if(head==0) {
-				return tmp.getName();
-			}else if(head==1&&c==c1) {
-				return tmp.getName();
-			}else if(head==2&&c==n) {
-				return tmp.getName();
+		while(current!=null) {
+			if(head=='0') {
+				return current.getName();
+			}else if(head=='1'&&c==c1) {
+				return current.getName();
+			}else if(head=='2'&&c==n) {
+				return current.getName();
 			}
-			tmp=tmp.getNext();
+			current=current.getNext();
 			c++;
 		}
 		return '#';
 	}
-	public void add(int head, char name) {
+	public void add(char head, char name) {
 		Tape add=new Tape(name);
-		Tape tmp = first;
+		Tape current = first;
 		if(first==null) {
 			first=add;
+			n++;
 		}else {
 			int c1=n==0?0:n/2;
 			int c = 0;
-			while(tmp!=null) {
-				if(head==0) {
-					add.setNext(tmp);
+			while(current!=null) {
+				if(head=='0') {
+					add.setNext(current);
 					first=add;
 					n++;
 					break;
-				}else if(head==1&&c==c1) {
-					Tape a=tmp.getNext();
-					tmp.setNext(add);
-					add.setNext(a);
+				}else if(head=='1'&&c==c1) {
+					Tape next=current.getNext();
+					current.setNext(add);
+					add.setNext(next);
 					n++;
 					break;
-				}else if(head==2&&tmp.getNext()==null){
-					tmp.setNext(add);
-					n++
+				}else if(head=='2'&&current.getNext()==null){
+					current.setNext(add);
+					n++;
+					break;
 				}
-				tmp=tmp.getNext();
+				current=current.getNext();
 				c++;
 			}
 		}
 	}
+	public void delete(char head) {
+		Tape prev=null;
+		Tape current=first;
+		Tape next=current.getNext();
+		int c1=n==0?0:n/2;
+		int c=0;
+		while(current!=null) {
+			if(head=='0') {
+				first=next;
+				n--;
+				break;
+			}else if(head=='1'&&c==c1) {
+				if(prev==null) {
+					first=next;
+				}else {
+					prev.setNext(next);
+				}
+				n--;
+				break;
+			}else if(head=='2'&&next==null) {
+				current=null;
+				n--;
+				break;
+			}
+			prev=current;
+			current=current.getNext();
+			next=next.getNext();
+			c++;
+		}
+	}
+	*/
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,5 +1,56 @@
 package ui;
+import java.io.*;
+
+import model.TuringMachine;
 
 public class Main {
-
+	private BufferedReader br;
+	private BufferedWriter bw;
+	private TuringMachine tm;
+	public Main() throws FileNotFoundException,IOException {
+		br=new BufferedReader(new FileReader("data"+File.separator+"input.txt"));
+		bw=new BufferedWriter(new FileWriter("data"+File.separator+"out_turing.txt"));
+		tm=new TuringMachine();
+	}
+	public void init() {}
+	public static void main(String[] args) {
+		try {
+			long start=System.currentTimeMillis();
+			Main main = new Main();
+			String s=null;
+			String toWrite="";
+			s=main.br.readLine();
+			while(s!=null) {
+				char head='3';
+				int action=-1;
+				int sg=-1;
+				for(int i=0;i<s.length();i++) {
+					sg++;
+					if(sg==0) {
+						head=s.charAt(i);
+					}else if(sg==1) {
+						action=s.charAt(i);
+						if(action=='0') {
+							toWrite+=main.tm.read(head)+"\n";
+						}else if(action=='2') {
+							main.tm.delete(head);
+						}
+						if(action!='1')sg=-1;
+					}else if(sg==2) {
+						main.tm.add(head,s.charAt(i));
+						sg=-1;
+					}
+				}
+				s=main.br.readLine();
+			}
+			main.bw.write(toWrite);
+			Thread.sleep(2000);
+			long end=System.currentTimeMillis();
+			System.out.println(end-start);
+			main.br.close();
+			main.bw.close();
+		}catch(Exception e) {
+			System.err.println(e.getMessage());
+		}
+	}
 }
